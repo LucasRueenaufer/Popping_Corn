d76476522119cbde86dc31f29140737170413cf4
LucasRueenaufer <lucas@rueenaufer.de>
2026-04-19 13:45:06 +0200

    Some renaming of variables

--- src/analysis.py
+++ src/analysis.py
@@ -18,7 +18,7 @@ import matplotlib.pyplot as plt
 from pathlib import Path
 
 data_folder =Path("../data/")
-generate_frec_plots = False
+generate_frec_plots = True
 
 def extract_data_from_mp3(path):
     """
@@ -44,33 +44,57 @@ def extract_data_from_mp3(path):
     waveform, sample_rate = librosa.load(path)
     energy = librosa.feature.rms(y=waveform,frame_length=frame_length, hop_length=hop_length)[0]
     
-    #additional frec plots by generating timing
-    times = librosa.frames_to_time(np.arange(len(energy)),sr=sample_rate,hop_length=hop_length)
     
-    # print energy over time
-    if generate_frec_plots:
-        plt.plot(times,energy)
-        plt.show()
-        
-   
     
-    # finding peaks using scipy. Choose peak-threshold of 4 times over mean
+    
+    # finding peaks using scipy. Choose peak-threshhold of 4 times over mean
     threshhold = np.mean(energy) * 4
     peaks = find_peaks(energy, height=threshhold)[0]
-    times = librosa.frames_to_time(peaks, sr=sample_rate)
+    times = librosa.frames_to_time(peaks, sr=sample_rate, hop_length=hop_length)
+    
+    # optionally print energy over time with threshhold
+    times_energy = librosa.frames_to_time(np.arange(len(energy)),sr=sample_rate,hop_length=hop_length)
+    if generate_frec_plots:
+        plt.plot(times_energy,energy)
+        plt.axhline(y=threshhold, color='r', linestyle='--')
+        plt.show()
+        
     return times
 
 def data_analysis():
+    """
+    SUMMARY.
+
+    Returns
+    -------
+    None
+    """
+    # find my mp3 files
+
     mp3_files = list(data_folder.glob("*.mp3"))
+    
+    #big data extraction for each file
     for file_path in mp3_files:
+        
+        # extract time data of popping events
         print("Processing:", file_path.name)
         timestamp_events = extract_data_from_mp3(file_path)
-        plt.hist(timestamp_events,cumulative=True,bins=20)
+        
+        # create gaussian fit to data
+        fit_time = np.linspace(min(timestamp_events),max(timestamp_events),1024)
+        fit_mean, fit_sigma = norm.fit(timestamp_events)
+        print(fit_mean,fit_sigma)
+        
+        #plot histogram
+        plt.hist(timestamp_events,bins=20,density=True,label="Registered Pops")
+ 
+        #plot fit
         plt.title(f"Popping against time for {file_path.stem}",)
-        plt.xlabel("Time in s")
-        plt.ylabel("Pop Events")
-        plt.show()
-        plt.hist(np.diff(timestamp_events),bins=10)
+        plt.xlabel("Time to Pop in s")
+        plt.ylabel("Frequency of Pops")
+        plt.plot(fit_time,norm.pdf(fit_time,fit_mean,fit_sigma),label=f"Best Fit $N({fit_mean:.2f},{fit_sigma:.2f})$")
+        plt.legend()
+        
         plt.show()
     
     pass
