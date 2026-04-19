f9c6a69f9fd19f640b99624e243963fcb9045b71
LucasRueenaufer <lucas@rueenaufer.de>
2026-04-19 14:44:55 +0200

    changed fit to right truncated gaussian

--- src/analysis.py
+++ src/analysis.py
@@ -14,11 +14,12 @@ import librosa
 import numpy as np
 from scipy.signal import find_peaks
 from scipy.stats import norm
+from scipy.optimize import minimize
 import matplotlib.pyplot as plt
 from pathlib import Path
 
 data_folder =Path("../data/")
-generate_frec_plots = True
+generate_frec_plots = False
 
 def extract_data_from_mp3(path):
     """
@@ -63,7 +64,7 @@ def extract_data_from_mp3(path):
 
 def data_analysis():
     """
-    SUMMARY.
+    Runs the main analysis of the data on all datasets.
 
     Returns
     -------
@@ -80,10 +81,11 @@ def data_analysis():
         print("Processing:", file_path.name)
         timestamp_events = extract_data_from_mp3(file_path)
         
-        # create gaussian fit to data
+        # create time-array for plotting
         fit_time = np.linspace(min(timestamp_events),max(timestamp_events),1024)
-        fit_mean, fit_sigma = norm.fit(timestamp_events)
-        print(fit_mean,fit_sigma)
+        
+        # Call for fit on right truncated gaussian
+        fit_parameters=fit_trunc_gaussian(timestamp_events)
         
         #plot histogram
         plt.hist(timestamp_events,bins=20,density=True,label="Registered Pops")
@@ -92,12 +94,92 @@ def data_analysis():
         plt.title(f"Popping against time for {file_path.stem}",)
         plt.xlabel("Time to Pop in s")
         plt.ylabel("Frequency of Pops")
-        plt.plot(fit_time,norm.pdf(fit_time,fit_mean,fit_sigma),label=f"Best Fit $N({fit_mean:.2f},{fit_sigma:.2f})$")
+        plt.plot(fit_time,trunc_gaussian(fit_time,fit_parameters),label=f"Best Fit for \n mu={fit_parameters[0]:.2f}, sig={fit_parameters[1]:.2f}")
         plt.legend()
         
         plt.show()
     
     pass
 
+def fit_trunc_gaussian(timestamp_events):
+    """
+    SUMMARY.
+
+    Parameters
+    ----------
+    timestamp_events : TYPE
+        DESCRIPTION.
+
+    Returns
+    -------
+    tuple : (mean, sigma)
+    """
+    
+    # select starting parameters for minimization by naive gaussian fit
+   
+    mean_init, sigma_init = norm.fit(timestamp_events)
+    
+    # Minimize negative log-likelihood through scipy.minimize
+    result = minimize(
+        negative_log_likelihood,
+        x0=[mean_init,sigma_init],
+        args=(timestamp_events),
+        bounds=[(None, None), (1e-6, None)]
+        )
+    
+    return result.x #return results of minimization
+
+def trunc_gaussian(x,parameters):
+    """
+    Just gives back a gaussian, which is truncated at the right side
+    (highest value for x)
+
+    Parameters
+    ----------
+    x : TYPE
+        array of x-axis values
+    parameters : TYPE
+        mean and sigma of the gaussian
+    Returns
+    -------
+    TYPE
+        DESCRIPTION.
+    """
+    #extract parameters
+    mean, sigma = parameters
+    
+    #return right truncated gaussian
+    return norm.pdf(x, mean, sigma)/norm.cdf((np.max(x)-mean)/sigma)
+
+def negative_log_likelihood(parameters,timestamps_events):
+    """
+    Returns the negative log-likelihood for a truncated gaussian
+
+    Parameters
+    ----------
+    parameters : TYPE
+        mean and sigma
+    timestamps_events : TYPE
+        timestamp data
+
+    Returns
+    -------
+    returns float with neg log likelihood
+    """
+    # extract parameters
+    mean, sigma = parameters
+    
+    # small sanity check
+    if sigma <=0:
+        print("Something went wrong while fitting.")
+        pass
+    
+    # pass parameters to pdf ad cdf function
+    log_pdf = norm.logpdf(timestamps_events, mean, sigma)
+    log_cdf = norm.logcdf((np.max(timestamps_events)-mean)/sigma)
+    
+    return -(np.sum(log_pdf)-len(timestamps_events)*log_cdf) #return the negative
+    # of the logarithm of the log likelihood
+    
 data_analysis()
 
